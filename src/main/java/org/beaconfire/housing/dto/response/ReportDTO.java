e86088b9c222fae8a01b33b0626fdaa201f1d567
xingyue-umich <xingyuez@umich.edu>
2025-07-12 02:32:30 -0400

    add DTOs and exception handlers

---
+++ src/main/java/org/beaconfire/housing/dto/response/ReportDTO.java
@@ -0,0 +1,22 @@
+package org.beaconfire.housing.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReportDTO {
+    private Integer id;
+    private String facilityName;  // e.g., "Chair"
+    private String title;
+    private String description;
+    private String status;  // "OPEN", "IN_PROGRESS", "CLOSED"
+    private Integer createdBy;  // Employee ID who created it
+    private Timestamp createDate;
+}
